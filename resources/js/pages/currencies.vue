ab5427f7ec32abf13bcf8eb9fc4cd13dc521360d
Evgeniy <ekovalwork@gmail.com>
2019-11-21 11:36:39 +0200

    Currency rates page

--- resources/js/pages/currencies.vue
+++ resources/js/pages/currencies.vue
@@ -4,7 +4,12 @@
       <div class="title mb-4 text-center">
         Currencies rates
       </div>
-      <div class="row">
+      <div v-if="loading" class="text-center">
+        <div class="spinner-border" role="status">
+          <span class="sr-only"></span>
+        </div>
+      </div>
+      <div v-else class="row">
         <div class="col-12">
           <div class="card">
             <div class="card-body">
@@ -17,12 +22,12 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="(currency, index, key) in currencies">
-                    <th scope="row">{{ key + 1 }}</th>
+                  <tr v-for="(currency, index, key) in currencies"> 
+                    <th scope="row">{{ index + 1 }}</th>
                     <td>
-                      <span>{{ currency.base }}</span>
+                      <span>{{ currency.data.base }}</span>
                     </td>
-                    <td>{{ currency.amount }} {{ currency.currency }}</td>
+                    <td>{{ currency.data.amount }} {{ currency.data.currency }}</td>
                   </tr>
                 </tbody>
               </table>
@@ -40,6 +45,10 @@ import { mapGetters } from 'vuex'
 export default {
   layout: 'default',
 
+  data: () => ({
+    loading: true
+  }),
+
   computed: {
     ...mapGetters({
         currencies: 'currencies/currenciesAll'
@@ -47,7 +56,10 @@ export default {
   },
 
   created() {
-    this.$store.dispatch('currencies/fetchCurrenciesAll');
+    this.$store.dispatch('currencies/fetchCurrenciesAll')
+      .then(() => {
+        this.loading = false;
+      });
   }
 }
 </script>
